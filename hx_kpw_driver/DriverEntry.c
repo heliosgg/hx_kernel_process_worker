37ca8cfc2db9901068ad413771814df0c14077ef
Андрей <Andrey22.02@mail.ru>
2020-01-12 18:56:27 +0300

    [init]: add driver, target

---
+++ hx_kpw_driver/DriverEntry.c
@@ -0,0 +1,167 @@
+#pragma warning(disable:6011)
+
+#include "ntos.h"
+
+#define DBG_MARKER "[HxKPW] "
+
+#define IO_READ_REQUEST CTL_CODE(FILE_DEVICE_UNKNOWN, 0x0701 /* Our Custom Code */, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+#define IO_WRITE_REQUEST CTL_CODE(FILE_DEVICE_UNKNOWN, 0x0702 /* Our Custom Code */, METHOD_BUFFERED, FILE_SPECIAL_ACCESS)
+
+PDEVICE_OBJECT pDeviceObject; // our driver object
+UNICODE_STRING dev, dos; // Driver registry paths
+
+// datatype for read request
+typedef struct _KERNEL_READ_REQUEST
+{
+	ULONG ProcessId;
+
+	ULONG Address;
+	ULONG Response;
+	ULONG Size;
+
+} KERNEL_READ_REQUEST, * PKERNEL_READ_REQUEST;
+
+typedef struct _KERNEL_WRITE_REQUEST
+{
+	ULONG ProcessId;
+
+	ULONG Address;
+	ULONG Value;
+	ULONG Size;
+
+} KERNEL_WRITE_REQUEST, * PKERNEL_WRITE_REQUEST;
+
+NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject);
+NTSTATUS CreateCall(PDEVICE_OBJECT DeviceObject, PIRP irp);
+NTSTATUS CloseCall(PDEVICE_OBJECT DeviceObject, PIRP irp);
+
+NTSTATUS KeReadVirtualMemory(PEPROCESS Process, PVOID SourceAddress, PVOID TargetAddress, SIZE_T Size)
+{
+	PSIZE_T Bytes;
+	if (NT_SUCCESS(MmCopyVirtualMemory(Process, SourceAddress, PsGetCurrentProcess(),
+		TargetAddress, Size, KernelMode, &Bytes)))
+		return STATUS_SUCCESS;
+	else
+		return STATUS_ACCESS_DENIED;
+}
+
+NTSTATUS KeWriteVirtualMemory(PEPROCESS Process, PVOID SourceAddress, PVOID TargetAddress, SIZE_T Size)
+{
+	PSIZE_T Bytes;
+	if (NT_SUCCESS(MmCopyVirtualMemory(PsGetCurrentProcess(), SourceAddress, Process,
+		TargetAddress, Size, KernelMode, &Bytes)))
+		return STATUS_SUCCESS;
+	else
+		return STATUS_ACCESS_DENIED;
+}
+
+// IOCTL Call Handler function
+NTSTATUS IoControl(PDEVICE_OBJECT DeviceObject, PIRP Irp)
+{
+	NTSTATUS Status;
+	ULONG BytesIO = 0;
+
+	PIO_STACK_LOCATION stack = IoGetCurrentIrpStackLocation(Irp);
+
+	// Code received from user space
+	ULONG ControlCode = stack->Parameters.DeviceIoControl.IoControlCode;
+
+	if (ControlCode == IO_READ_REQUEST)
+	{
+		// Get the input buffer & format it to our struct
+		PKERNEL_READ_REQUEST ReadInput = (PKERNEL_READ_REQUEST)Irp->AssociatedIrp.SystemBuffer;
+		PKERNEL_READ_REQUEST ReadOutput = (PKERNEL_READ_REQUEST)Irp->AssociatedIrp.SystemBuffer;
+
+		PEPROCESS Process;
+		// Get our process
+		if (NT_SUCCESS(PsLookupProcessByProcessId(ReadInput->ProcessId, &Process)))
+			KeReadVirtualMemory(Process, ReadInput->Address,
+				&ReadInput->Response, ReadInput->Size);
+
+		DbgPrintEx(0, 0, DBG_MARKER "Read Params:  %lu, %#010x \n", ReadInput->ProcessId, ReadInput->Address);
+		DbgPrintEx(0, 0, DBG_MARKER "Value: %lu \n", ReadOutput->Response);
+
+		Status = STATUS_SUCCESS;
+		BytesIO = sizeof(KERNEL_READ_REQUEST);
+	}
+	else if (ControlCode == IO_WRITE_REQUEST)
+	{
+		// Get the input buffer & format it to our struct
+		PKERNEL_WRITE_REQUEST WriteInput = (PKERNEL_WRITE_REQUEST)Irp->AssociatedIrp.SystemBuffer;
+
+		PEPROCESS Process;
+		// Get our process
+		if (NT_SUCCESS(PsLookupProcessByProcessId(WriteInput->ProcessId, &Process)))
+			KeWriteVirtualMemory(Process, &WriteInput->Value,
+				WriteInput->Address, WriteInput->Size);
+
+		DbgPrintEx(0, 0, DBG_MARKER "Write Params:  %lu, %#010x \n", WriteInput->Value, WriteInput->Address);
+
+		Status = STATUS_SUCCESS;
+		BytesIO = sizeof(KERNEL_WRITE_REQUEST);
+	}
+	else
+	{
+		// if the code is unknown
+		Status = STATUS_INVALID_PARAMETER;
+		BytesIO = 0;
+	}
+
+	// Complete the request
+	Irp->IoStatus.Status = Status;
+	Irp->IoStatus.Information = BytesIO;
+	IoCompleteRequest(Irp, IO_NO_INCREMENT);
+
+	return Status;
+}
+
+// Driver Entrypoint
+NTSTATUS DriverEntry(PDRIVER_OBJECT pDriverObject,
+	PUNICODE_STRING pRegistryPath)
+{
+	DbgPrintEx(0, 0, DBG_MARKER "Driver Loaded\n");
+
+	RtlInitUnicodeString(&dev, L"\\Device\\hx_kernel_process_worker");
+	RtlInitUnicodeString(&dos, L"\\DosDevices\\hx_kernel_process_worker");
+
+	IoCreateDevice(pDriverObject, 0, &dev, FILE_DEVICE_UNKNOWN, FILE_DEVICE_SECURE_OPEN, FALSE, &pDeviceObject);
+	IoCreateSymbolicLink(&dos, &dev);
+
+	pDriverObject->MajorFunction[IRP_MJ_CREATE] = CreateCall;
+	pDriverObject->MajorFunction[IRP_MJ_CLOSE] = CloseCall;
+	pDriverObject->MajorFunction[IRP_MJ_DEVICE_CONTROL] = IoControl;
+	pDriverObject->DriverUnload = UnloadDriver;
+
+	pDeviceObject->Flags |= DO_DIRECT_IO;
+	pDeviceObject->Flags &= ~DO_DEVICE_INITIALIZING;
+
+	return STATUS_SUCCESS;
+}
+
+
+
+NTSTATUS UnloadDriver(PDRIVER_OBJECT pDriverObject)
+{
+	DbgPrintEx(0, 0, DBG_MARKER "Unload routine called.\n");
+
+	IoDeleteSymbolicLink(&dos);
+	IoDeleteDevice(pDriverObject->DeviceObject);
+}
+
+NTSTATUS CreateCall(PDEVICE_OBJECT DeviceObject, PIRP irp)
+{
+	irp->IoStatus.Status = STATUS_SUCCESS;
+	irp->IoStatus.Information = 0;
+
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
+
+NTSTATUS CloseCall(PDEVICE_OBJECT DeviceObject, PIRP irp)
+{
+	irp->IoStatus.Status = STATUS_SUCCESS;
+	irp->IoStatus.Information = 0;
+
+	IoCompleteRequest(irp, IO_NO_INCREMENT);
+	return STATUS_SUCCESS;
+}
---
+++ hx_target_process/main.cpp
@@ -0,0 +1,22 @@
+#include <string.h>
+#include <stdio.h>
+#include <iostream>
+#include <Windows.h>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+   DWORD a = 0xaabbccdd;
+
+   printf("Pid: %08x\n", GetCurrentProcessId());
+   printf("Address of \"a\" is %08x\n", (DWORD)&a);
+
+   while (true)
+   {
+      printf("Value of \"a\" is %08x\n", a);
+      Sleep(1000);
+   }
+
+   return 0;
+}
